49f50a3973c0faa33780704eaad9962bb92993e5
Rabin Mallick <rabinmallick@ymail.com>
2019-01-14 17:33:06 +0600

    port

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -488,7 +488,7 @@ app.post('/', function (req, res) {
 
 });
 
-app.listen(3000, function () {
-    console.log('Development endpoint listening on port 3000!');
+app.listen(8000, function () {
+    console.log('Development endpoint listening on port 8000!');
 });
 
